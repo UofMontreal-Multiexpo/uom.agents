5447dc5b23f1007df154be123b1910f8cfa81ff0
Gauthier Magnin <gauthier.magnin@gmail.com>
2024-05-15 12:08:17 +0200

    chore: Add package related files

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# uom.agents 0.0.0.9000-0 (development version)
+
+
+
+---
---
+++ R/uom.agents-package.R
@@ -0,0 +1,11 @@
+#' @details
+#' For an organized list of datasets and functions, use
+#'  \code{vignette("list_of_help_pages", "uom.agents")}
+#' 
+#' For an alphabetical list and for the package description, use
+#'  \code{library(help = "uom.agents")}
+#' 
+#' For package news information, use \code{news(package = "uom.agents")}
+#' 
+#' For information on how to cite the package, use \code{citation("uom.agents")}
+"_PACKAGE"
